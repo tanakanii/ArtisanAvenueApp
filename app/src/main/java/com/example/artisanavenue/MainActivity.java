05184753539c2249bbd70846ea02eb65eeead4ab
papi bel <joseph.beltran1314@gmail.com>
2024-05-06 13:03:57 +0800

    update mainact to menu and mainmenu to signin

--- app/src/main/java/com/example/artisanavenue/MainActivity.java
+++ app/src/main/java/com/example/artisanavenue/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.artisanavenue;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.artisanavenue.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Home());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new Home());
+            } else if (itemId == R.id.products) {
+                replaceFragment(new Products());
+            } else if (itemId == R.id.artisans) {
+                replaceFragment(new Artisans());
+            } else if (itemId == R.id.blogs) {
+                replaceFragment(new Blogs());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new Profile());
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/artisanavenue/SignUp.java
+++ app/src/main/java/com/example/artisanavenue/SignUp.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainMenu extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_signup);
     }
 }
