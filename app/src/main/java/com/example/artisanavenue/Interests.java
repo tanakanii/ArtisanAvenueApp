72bbabb8df949c62abc4c5641f53415269c9fe5f
pawtato <kyleangelacatiis27@gmail.com>
2024-05-07 00:52:15 +0800

    ฅ^.ᆺ.^ฅ

--- app/src/main/java/com/example/artisanavenue/Interests.java
+++ app/src/main/java/com/example/artisanavenue/Interests.java
@@ -15,7 +15,7 @@ public class Interests extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_interests);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
