5cae7ba87d83c4403318c8a59cd530c0c47d9027
papi bel <joseph.beltran1314@gmail.com>
2024-05-06 11:54:46 +0800

    navbar

--- app/src/main/java/com/example/artisanavenue/signin.java
+++ app/src/main/java/com/example/artisanavenue/signin.java
@@ -1,8 +1,8 @@
 package com.example.artisanavenue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class signin extends AppCompatActivity {
 
